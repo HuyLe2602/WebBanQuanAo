ae35d51233f1c5ab7ffc345a91e7ec0e8674bcd3
HuyLe2602 <yang260204@gmail.com>
2026-04-14 21:31:54 +0700

    em gui bai 14/4

---
+++ BanHangOnline/Models/EF/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BanHangOnline.Models.EF
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string seoTitle { get; set; }
+
+        public string seoDescription { get; set; }
+
+        public string seoKeywords { get; set; }
+        
+        public string Position { get; set; }
+    }
+}
