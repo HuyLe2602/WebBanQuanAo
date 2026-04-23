3ed522d80e93e9a421cf433f26d8cfc5167f3cc2
HuyLe2602 <yang260204@gmail.com>
2026-04-23 20:09:45 +0700

    em gui bai 23/4

---
+++ BanHangOnline/Areas/Admin/Controllers/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using BanHangOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanHangOnline.Areas.Admin.Controllers
+{
+    public class MenuController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MenuTop()
+        {
+            var items = db.Categories.OrderBy(x => x.Position).ToList();
+            return PartialView("_MenuTop", items);
+        }
+    }
+}
